cdfd762e4782d72e1b3d5b62f92b7e3ddc4f7e9e
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2023-10-12 19:59:15 +0300

    ✨ feat:
    add validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+    validates :title, :description, :isbn, :description, :author, presence: true
+    validates :title, :description, :isbn, :description, uniqueness: true
+    validates :isbn, length: { in: 9..10 }
+    validates :isbn, numericality: { only_integer: true }
+
 end

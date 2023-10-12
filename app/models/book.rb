bf03009000aa9a2a7217dfaf0ff9751e48d41b69
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2023-10-12 20:02:56 +0300

    🦄 refactor:
    remove

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,6 @@
 class Book < ApplicationRecord
-    validates :title, :description, :isbn, :description, :author, presence: true
-    validates :title, :description, :isbn, :description, uniqueness: true
+    validates :title, :isbn, :description, :author, presence: true
+    validates :title, :isbn, :description, uniqueness: true
     validates :isbn, length: { in: 9..10 }
     validates :isbn, numericality: { only_integer: true }
-
 end

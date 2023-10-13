4389b6108b71ad8b8852e588b28f989a5385234b
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2023-10-13 12:23:05 +0300

    🐞 fix:
    isbn mast equal_to 13 dig.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,5 @@
 class Book < ApplicationRecord
     validates :title, :isbn, :description, :author, presence: true
     validates :title, :isbn, :description, uniqueness: true
-    validates :isbn, length: { in: 9..10 }
-    validates :isbn, numericality: { only_integer: true }
+    validates :isbn, numericality: { only_integer: true, equal_to: 13 }
 end

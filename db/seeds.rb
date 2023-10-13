20e61ed58ed09ab336c881c0f944af6fec55cdc0
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2023-10-13 14:09:13 +0300

    🔧 build:
    add faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+100.times do
+  Book.create(
+    title: Faker::Book.title,
+    author: Faker::Book.author,
+    isbn: Faker::Number.decimal_part(digits: 13),
+    description: Faker::Lorem.paragraph
+  )
+end

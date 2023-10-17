ef9d5aa1bcb2ba633e569b6ddf9a85ff2bffc0cf
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2023-10-17 14:16:14 +0300

    🐞 fix:
    rebuld controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,58 +1,49 @@
-# frozen_string_literal: true
-
 class BooksController < ApplicationController
-  before_action :set_book, only: %i[show edit update destroy]
-
-  # GET /books
   def index
-    @books = Book.all
+    @books = Book.order(:id)
   end
 
-  # GET /books/1
-  def show; end
+  def show
+    @book = Book.find(params[:id])
+  end
 
-  # GET /books/new
   def new
     @book = Book.new
   end
 
-  # GET /books/1/edit
-  def edit; end
+  def edit
+    @book = Book.find(params[:id])
+  end
 
-  # POST /books
   def create
     @book = Book.new(book_params)
 
     if @book.save
-      redirect_to @book, notice: 'Book was successfully created.'
+      redirect_to book_path(@book), notice: "Book was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def update
+    @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to @book, notice: 'Book was successfully updated.', status: :see_other
+      redirect_to book_path(@book), notice: "Book was successfully updated."
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /books/1
   def destroy
-    @book.destroy!
-    redirect_to books_url, notice: 'Book was successfully destroyed.', status: :see_other
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: 'Book was successfully destroyed.',
+      status: :see_other
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_book
-    @book = Book.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def book_params
-    params.require(:book).permit(:title, :author, :isbn, :description)
-  end
+    def book_params
+      params.require(:book).permit(:title, :author, :isbn, :description)
+    end
 end

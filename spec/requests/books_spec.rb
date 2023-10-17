989cbd78c935220e453d291f0efe5d467c2889e0
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2023-10-17 15:33:25 +0300

    🐞 fix:
    rewrite test

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -8,71 +8,58 @@ RSpec.describe BooksController, type: :request do
 
   describe 'POST #create' do
     context 'with valid parameters' do
-      it 'creates a new Book' do
+      it 'creates a new Book and redirects to the created book' do
         expect do
           post books_url, params: { book: valid_attributes }
         end.to change(Book, :count).by(1)
-      end
 
-      it 'redirects to the created book' do
-        post books_url, params: { book: valid_attributes }
         expect(response).to redirect_to(book_url(Book.last))
       end
     end
 
     context 'with invalid parameters' do
-      it 'does not create a new Book' do
+      it 'does not create a new Book and renders a response with 422 status' do
         expect do
           post books_url, params: { book: invalid_attributes }
         end.to change(Book, :count).by(0)
-      end
 
-      it 'renders a response with 422 status (i.e., to display the "new" template)' do
-        post books_url, params: { book: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
 
-  describe 'PATCH /update' do
+  describe 'PATCH #update' do
     let(:book) { create(:book) }
     let(:new_attributes) { attributes_for(:book) }
 
     context 'with valid parameters' do
-      it 'updates the requested book' do
-        patch book_url(book), params: { book: new_attributes }
-        book.reload
-        expect(book.title).to eq(new_attributes[:title])
-        expect(book.author).to eq(new_attributes[:author])
-      end
+      it 'updates the requested book and redirects to the book' do
+        expect {
+          patch book_url(book), params: { book: new_attributes }
+          book.reload
+        }.to change { book.title }.to(new_attributes[:title])
+         .and change { book.author }.to(new_attributes[:author])
 
-      it 'redirects to the book' do
-        patch book_url(book), params: { book: new_attributes }
-        book.reload
         expect(response).to redirect_to(book_url(book))
       end
     end
 
     context 'with invalid parameters' do
-      it 'renders a response with 422 status (i.e., to display the "edit" template)' do
+      it 'renders a response with 422 status' do
         patch book_url(book), params: { book: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
 
-  describe 'DELETE /destroy' do
+  describe 'DELETE #destroy' do
     let!(:book) { create(:book) }
 
-    it 'destroys the requested book' do
+    it 'destroys the requested book and redirects to the books list' do
       expect do
         delete book_url(book)
       end.to change(Book, :count).by(-1)
-    end
 
-    it 'redirects to the books list' do
-      delete book_url(book)
-      
       expect(response).to redirect_to(books_url)
     end
   end

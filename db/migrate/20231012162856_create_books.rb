9737535caa774e07187827d734febff776c77c12
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2023-10-17 21:44:58 +0300

    🐞 fix:
    add , null: false to migration

--- db/migrate/20231012162856_create_books.rb
+++ db/migrate/20231012162856_create_books.rb
@@ -3,10 +3,10 @@
 class CreateBooks < ActiveRecord::Migration[7.1]
   def change
     create_table :books do |t|
-      t.string :title
-      t.string :author
-      t.string :isbn
-      t.text :description
+      t.string :title, null: false
+      t.string :author, null: false
+      t.string :isbn, null: false
+      t.text :description, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,10 +15,10 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_12_162856) do
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
-    t.string "isbn"
-    t.text "description"
+    t.string "title", null: false
+    t.string "author", null: false
+    t.string "isbn", null: false
+    t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

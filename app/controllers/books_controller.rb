5bb0657d43a74c68b4a5fbd4e9cfb3dbfaef446e
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2023-11-12 21:20:57 +0200

    The diff is about changing the method names from `collection` to `resourse` and `book_params` to `resourse_params` in the `BooksController` class.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -43,15 +43,15 @@ class BooksController < ApplicationController
 
   private
 
-    def collection
-      Book.ordered
-    end
+  def collection
+    Book.ordered
+  end
 
-    def resourse
-      collection.find(params[:id])
-    end
+  def resourse
+    collection.find(params[:id])
+  end
 
-    def book_params
-      params.require(:book).permit(:title, :author, :isbn, :description, :pdf)
-    end
+  def book_params
+    params.require(:book).permit(:title, :author, :isbn, :description, :pdf)
+  end
 end
